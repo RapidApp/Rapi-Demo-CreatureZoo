cb6013f7672844572ff75ec605071fbfc708dd09
Henry Van Styn <vanstyn@intellitree.com>
2015-05-12 17:45:22 -0400

    Bare-bones, AppDV-based CreaturePage

--- lib/Rapi/Demo/CreatureZoo/Module/CreaturePage.pm
+++ lib/Rapi/Demo/CreatureZoo/Module/CreaturePage.pm
@@ -4,12 +4,26 @@ use strict;
 use warnings;
 
 use Moose;
-extends 'RapidApp::Module::DbicPropPage';
+extends 'RapidApp::Module::AppDV';
+with 'RapidApp::Module::StorCmp::Role::DbicLnk::RowPg';
 
 use RapidApp::Util qw(:all);
+use Path::Class qw(file dir);
 
-# TODO
 
+sub BUILD {
+  my $self = shift;
+  $self->apply_extconfig( itemSelector => 'div.dvitem' );
+}
+
+has '+allow_restful_queries', default => 1;
+
+has '+tt_include_path', default => sub {
+  my $self = shift;
+  dir( $self->app->ra_builder->share_dir )->stringify;
+};
+
+has '+tt_file', default => 'templates/creature.html';
 
 before 'content' => sub {
   my $self = shift;
