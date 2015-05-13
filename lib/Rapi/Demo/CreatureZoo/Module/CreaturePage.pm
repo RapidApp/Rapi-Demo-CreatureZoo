5d543d3cbfe7ef69fc146c609846a17254c5432d
Henry Van Styn <vanstyn@intellitree.com>
2015-05-12 20:17:04 -0400

    CreaturePage now uses new DbicRowDV module

--- lib/Rapi/Demo/CreatureZoo/Module/CreaturePage.pm
+++ lib/Rapi/Demo/CreatureZoo/Module/CreaturePage.pm
@@ -4,32 +4,24 @@ use strict;
 use warnings;
 
 use Moose;
-extends 'RapidApp::Module::AppDV';
-with 'RapidApp::Module::StorCmp::Role::DbicLnk::RowPg';
+extends 'RapidApp::Module::DbicRowDV';
 
 use RapidApp::Util qw(:all);
 use Path::Class qw(file dir);
 
-
-sub BUILD {
-  my $self = shift;
-  $self->apply_extconfig( itemSelector => 'div.dvitem' );
-}
-
-has '+allow_restful_queries', default => 1;
+has '+template', default => 'templates/creature.html';
 
 has '+tt_include_path', default => sub {
   my $self = shift;
   dir( $self->app->ra_builder->share_dir )->stringify;
 };
 
-has '+tt_file', default => 'templates/creature.html';
-
 before 'content' => sub {
   my $self = shift;
   
   $self->apply_extconfig( tabTitle => $self->req_Row->name );
 };
+
 
 1;
 
